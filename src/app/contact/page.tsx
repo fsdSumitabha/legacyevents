aab528f331c7c25e812463dc2f5ff1caf2695bb3
dSumitabha <fullstackdev@zanservices.com>
2026-02-27 19:18:39 +0530

    contact cta page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import FinalCTASection from "@/components/sections/FinalSectionCTA"
+
+
+export default function contactPage() {
+    return (
+        <main>
+            <FinalCTASection />
+        </main>
+    )
+}
