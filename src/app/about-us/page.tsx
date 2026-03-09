3f0d790017acec9e301256dd75570d61fcdfa69f
dSumitabha <fullstackdev@zanservices.com>
2026-03-09 20:32:41 +0530

    about-us page initated

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import BrandPhilosophy from "@/components/sections/BrandPhilosophy"
+import FinalCTASection from "@/components/sections/FinalSectionCTA"
+
+
+export default function AboutPage() {
+    return (
+        <main>
+            <BrandPhilosophy />
+            <FinalCTASection />
+        </main>
+    )
+}
